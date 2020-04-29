6aae5dd8afc6f9fb9f69f45966ef714ad1a78543
Erin Rodriguez <50300303+cooldoggy@users.noreply.github.com>
2020-04-29 01:00:58 -0400

    Finished Ask Expert Countries.

---
+++ askexpert.py
@@ -0,0 +1,27 @@
+from tkinter import Tk, simpledialog, messagebox
+def readfromfile():
+    with open('capitaldata.txt') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            country, city = line.split('/')
+            the_world[country]= city
+def writetofile(country_name, city_name):
+    with open('capitaldata.txt', 'a') as file:
+        file.write('\n' + country_name + '/' + city_name)
+root = Tk()
+root.withdraw()
+the_world={}
+readfromfile()
+while True:
+    query_country= simpledialog.askstring('Ask the Expert!- Capital Cities', "Type the name of a country:")
+    query_country=query_country.capitalize()
+
+    if query_country in the_world:
+        result = the_world[query_country]
+        messagebox.showinfo('Answer', 'The capital city of ' + query_country + ' is ' + result + '.')
+    else:
+        new_city = simpledialog.askstring('Teach Me!', "I don't know! " + "What is the capital city of " + query_country + '?')
+        the_world[query_country] = new_city
+        writetofile(query_country, new_city)
+        messagebox.showinfo('Thanks!', "Ok! The capital city of " + query_country + " is " + new_city + ".")
+root.mainloop()
